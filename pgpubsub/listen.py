879afefaa2b98d5f0afab3252c8691a478b6bcec
Nate <nathan.smeltzer@gmail.com>
2025-07-16 05:26:36 -0400

    add payload_filter and channel logs

--- pgpubsub/listen.py
+++ pgpubsub/listen.py
@@ -202,14 +202,16 @@ class LockableNotificationProcessor(NotificationProcessor):
             logger.info(f'Could not obtain a lock on notification '
                         f'{self.notification.pid}\n')
             # we'll assume this is the locked pgpubsub notification
-            locked_notification = (
+            notification_without_skip_locked = (
                 Notification.objects.select_for_update(
                     skip_locked=False).filter(
                     payload_filter,
                     channel=self.notification.channel,
                 ).first()
             )
-            logger.info(f"locked pgpubsub notification: {locked_notification}")
+            logger.info(f"locked pgpubsub notification: {notification_without_skip_locked}")
+            logger.info(f"payload filter: {payload_filter}")
+            logger.info(f"channel: {self.notification.channel}")
             logger.info(f"postgres notification payload {self.notification.payload}")
         else:
             logger.info(f'Obtained lock on {notification}')
